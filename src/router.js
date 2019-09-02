5c6486de826360147c13024117d2f1a9c2aa3c85
diskfan <48484343123@qq.com>
2019-09-02 13:06:59 +0800

    ts

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import header from './views/header.vue'
+
+const importModule = file => () => import(`@/views/${file}`)
 
 Vue.use(Router)
 
@@ -10,16 +11,39 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'header',
-      component: header
+      component: importModule('home/home')
     },
     {
-      path: '*',
-      name: '404',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/404.vue')
+      path: '/home',
+      name: 'home',
+      component: importModule('home/home')
+    }, {
+      path: '/about',
+      name: 'about',
+      component: importModule('about/about')
+    }, {
+      path:'/platform',
+      name: 'platform',
+      component: importModule('platform/platform')
+    }, {
+      path: '/product',
+      name: 'product',
+      component: importModule('product/product')
+    }, {
+      path: '/suport',
+      name: 'suport',
+      component: importModule('suport/suport')
+    }, {
+      path: '/*',
+      redirect: '/'
     }
+    // {
+    //   path: '*',
+    //   name: '404',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/404.vue')
+    // }
   ]
 })
---
+++ src/views/platform/platform.vue
@@ -0,0 +1,21 @@
+<!--
+ * @ Author: Diskfan
+ * @ Create Time: 2019-09-02 12:42:19
+ * @ Modified by: Your name
+ * @ Modified time: 2019-09-02 13:00:57
+ * @ Description: 交易平台
+ -->
+
+<template>
+  <div>
+    platform
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+  
+</style>
