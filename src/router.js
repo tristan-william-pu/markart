0fa849531cec241708f37146d75d04415c1c8c81
diskfan <48484343123@qq.com>
2019-09-02 16:51:34 +0800

    Modify Router

--- src/router.js
+++ src/router.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-const importModule = file => () => import(`@/views/${file}`)
-
+const importModule = file => () => import(`@/views/${file}.vue`)
 Vue.use(Router)
 
 export default new Router({
@@ -11,7 +10,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: importModule('home/home')
+      redirect: '/home'
     },
     {
       path: '/home',
@@ -23,113 +22,103 @@ export default new Router({
       component: importModule('about/safety')
     }, {
       // 交易平台
-      path:'/platform',
+      path: '/platform',
       name: 'platform',
-      component: importModule('platform/platform'),
-      children: [
-        {
-          // MT4
-          path: 'mt4',
-          name: 'mt4',
-          component: importModule('platform/mt4')
-        }, {
-          // 杠杆交易
-          path: 'leverage',
-          name: 'leverage',
-          component: importModule('platform/leverage')
-        }, {
-          path: 'account',
-          name: 'account',
-          component: importModule('platform/account')
-        }, {
-          // clearpro
-          path: 'clearpro',
-          name: 'clearpro',
-          component: importModule('platform/clearpro')
-        }, {
-          path: 'man',
-          name: 'man',
-          component: importModule('platform/man')
-        }
-      ]
+      redirect: '/mt4'
+    },
+    {
+      // MT4
+      path: '/mt4',
+      name: 'mt4',
+      component: importModule('platform/mt4')
+    }, {
+      // 杠杆交易
+      path: '/leverage',
+      name: 'leverage',
+      component: importModule('platform/leverage')
+    }, {
+      path: '/account',
+      name: 'account',
+      component: importModule('platform/account')
+    }, {
+      // clearpro
+      path: '/clearpro',
+      name: 'clearpro',
+      component: importModule('platform/clearpro')
+    }, {
+      path: '/man',
+      name: 'man',
+      component: importModule('platform/man')
     }, {
       path: '/product',
       name: 'product',
-      component: importModule('product/product'),
-      children: [
-        {
-          // 外汇
-          path: 'foreign',
-          name: 'foreign',
-          component: importModule('product/foreign')
-        }, {
-          // 贵金属
-          path: 'metal',
-          name: 'metal',
-          component: importModule('product/metal')
-        }, {
-          // 原油
-          path: 'oil',
-          name: 'oil',
-          component: importModule('product/oil')
-        }, {
-          // 差价合约
-          path: 'contract',
-          name: 'contract',
-          component: importModule('product/contract')
-        }, {
-          // 指数
-          path: 'tradeindex',
-          name: 'tradeindex',
-          component: importModule('product/tradeindex')
-        }, {
-          // 数字货币
-          path: 'currency',
-          name: 'currency',
-          component: importModule('product/currency')
-        }
-      ]
+      redirect: '/foreign'
+    }, {
+      // 外汇
+      path: '/foreign',
+      name: 'foreign',
+      component: importModule('product/foreign')
+    }, {
+      // 贵金属
+      path: '/metal',
+      name: 'metal',
+      component: importModule('product/metal')
+    }, {
+      // 原油
+      path: '/oil',
+      name: 'oil',
+      component: importModule('product/oil')
+    }, {
+      // 差价合约
+      path: '/contract',
+      name: 'contract',
+      component: importModule('product/contract')
+    }, {
+      // 指数
+      path: '/tradeindex',
+      name: 'tradeindex',
+      component: importModule('product/tradeindex')
+    }, {
+      // 数字货币
+      path: '/currency',
+      name: 'currency',
+      component: importModule('product/currency')
     }, {
       path: '/suport',
       name: 'suport',
-      component: importModule('suport/suport'),
-      children: [
-        {
-          path: 'type',
-          name: 'type',
-          component: importModule('suport/type')
-        }, {
-          // school
-          path: 'school',
-          name: 'school',
-          component: importModule('suport/school')
-        }
-      ]
+      redirect: '/type'
     }, {
-      path: 'partners',
+      path: '/type',
+      name: 'type',
+      component: importModule('suport/type')
+    }, {
+      // school
+      path: '/school',
+      name: 'school',
+      component: importModule('suport/school')
+    }, {
+      path: '/partners',
       name: 'partners',
       component: importModule('partners/partners'),
-      children: [
-        {
-          // 白标
-          path: 'white',
-          name: 'white',
-          component: importModule('partners/white')
-        }, {
-          // api
-          path: 'api',
-          name: 'api',
-          component: importModule('partners/api')
-        }, {
-          path: 'provider',
-          name: 'provider',
-          component: importModule('partners/provider')
-        }, {
-          path: 'ib',
-          name: 'ib',
-          component: importModule('partnets/ib')
-        }
-      ]
+      redirect: '/partners/white',
+    }, {
+      // 白标
+      path: '/white',
+      name: 'white',
+      component: importModule('partners/white')
+    }, {
+      // api
+      path: '/api',
+      name: 'api',
+      component: importModule('partners/api')
+    }, {
+      path: '/provider',
+      name: 'provider',
+      component: importModule('partners/provider')
+    }, {
+      path: '/ib',
+      name: 'ib',
+      component: importModule('partnets/ib')
     }, {
       path: '/contantus',
       name: 'contantus',
@@ -138,13 +127,5 @@ export default new Router({
       path: '/*',
       redirect: '/'
     }
-    // {
-    //   path: '*',
-    //   name: '404',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/404.vue')
-    // }
   ]
 })
--- src/views/product/foreign.vue
+++ src/views/product/foreign.vue
@@ -1,6 +1,25 @@
+<!--
+ * @ Author: Diskfan
+ * @ Create Time: 2019-09-02 13:55:38
+ * @ Modified by: Your name
+ * @ Modified time: 2019-09-02 16:50:02
+ * @ Description: 外汇
+ -->
+
+
 <template>
-  <div>
-    
+  <div class="foreign">
+    <div class="title">
+      外汇
+    </div>
+    <div class="exp">
+      <div class="leftimg">
+        
+      </div>
+      <div class="confent">
+
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -9,5 +28,16 @@ export default {
 }
 </script>
 <style lang="scss">
-  
+  @import '@/styles/mixin.scss';
+  .foreign {
+    >.title {
+      font-size: 75px;
+      color: #000;
+      line-height: 223px;
+      font-family: montserratse;
+      width: 100%;
+      text-align: center;
+      margin-top: 10px;
+    }
+  }
 </style>
