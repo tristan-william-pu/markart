7c781220c41ddfb9c0a786d738107fa53f0208e1
diskfan <48484343123@qq.com>
2019-09-02 14:21:39 +0800

    add files

--- src/router.js
+++ src/router.js
@@ -20,19 +20,120 @@ export default new Router({
     }, {
       path: '/about',
       name: 'about',
-      component: importModule('about/about')
+      component: importModule('about/safety')
     }, {
+      // 交易平台
       path:'/platform',
       name: 'platform',
-      component: importModule('platform/platform')
+      component: importModule('platform/platform'),
+      children: [
+        {
+          // MT4
+          path: 'mt4',
+          name: 'mt4',
+          component: importModule('platform/mt4')
+        }, {
+          // 杠杆交易
+          path: 'leverage',
+          name: 'leverage',
+          component: importModule('platform/leverage')
+        }, {
+          path: 'account',
+          name: 'account',
+          component: importModule('platform/account')
+        }, {
+          // clearpro
+          path: 'clearpro',
+          name: 'clearpro',
+          component: importModule('platform/clearpro')
+        }, {
+          path: 'man',
+          name: 'man',
+          component: importModule('platform/man')
+        }
+      ]
     }, {
       path: '/product',
       name: 'product',
-      component: importModule('product/product')
+      component: importModule('product/product'),
+      children: [
+        {
+          // 外汇
+          path: 'foreign',
+          name: 'foreign',
+          component: importModule('product/foreign')
+        }, {
+          // 贵金属
+          path: 'metal',
+          name: 'metal',
+          component: importModule('product/metal')
+        }, {
+          // 原油
+          path: 'oil',
+          name: 'oil',
+          component: importModule('product/oil')
+        }, {
+          // 差价合约
+          path: 'contract',
+          name: 'contract',
+          component: importModule('product/contract')
+        }, {
+          // 指数
+          path: 'tradeindex',
+          name: 'tradeindex',
+          component: importModule('product/tradeindex')
+        }, {
+          // 数字货币
+          path: 'currency',
+          name: 'currency',
+          component: importModule('product/currency')
+        }
+      ]
     }, {
       path: '/suport',
       name: 'suport',
-      component: importModule('suport/suport')
+      component: importModule('suport/suport'),
+      children: [
+        {
+          path: 'type',
+          name: 'type',
+          component: importModule('suport/type')
+        }, {
+          // school
+          path: 'school',
+          name: 'school',
+          component: importModule('suport/school')
+        }
+      ]
+    }, {
+      path: 'partners',
+      name: 'partners',
+      component: importModule('partners/partners'),
+      children: [
+        {
+          // 白标
+          path: 'white',
+          name: 'white',
+          component: importModule('partners/white')
+        }, {
+          // api
+          path: 'api',
+          name: 'api',
+          component: importModule('partners/api')
+        }, {
+          path: 'provider',
+          name: 'provider',
+          component: importModule('partners/provider')
+        }, {
+          path: 'ib',
+          name: 'ib',
+          component: importModule('partnets/ib')
+        }
+      ]
+    }, {
+      path: '/contantus',
+      name: 'contantus',
+      component: importModule('contantus/contantus')
     }, {
       path: '/*',
       redirect: '/'
---
+++ src/views/partners/partners.vue
@@ -0,0 +1,23 @@
+<!--
+ * @ Author: Diskfan
+ * @ Create Time: 2019-09-02 14:11:43
+ * @ Modified by: Your name
+ * @ Modified time: 2019-09-02 14:12:06
+ * @ Description: 合作伙伴
+ -->
+
+
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+
+export default {
+  
+}
+</script>
+<style lang="scss">
+  
+</style>
